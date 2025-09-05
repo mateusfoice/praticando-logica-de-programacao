e97d30dd2cf5dca8eb3f09b05e7ad661a8ffda0c
Mateus Alves Costa <mateuscosta3497@gmail.com>
2025-09-05 19:06:28 -0300

    multiplicando matrizes compatíveis

---
+++ praticandoJava/src/praticandoJava/MatrizCompatível.java
@@ -0,0 +1,65 @@
+package praticandoJava;
+
+import java.util.Scanner;
+
+public class MatrizCompatível {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int i = 0;
+		int j = 0;
+		
+		do {
+		System.out.print("Define o tamanho da linha do primeiro vetor: ");
+		i = sc.nextInt();
+		
+		System.out.print("Define o tamanho da coluna do segundo vetor: ");
+		j = sc.nextInt();
+		
+		if(i!=j) {
+			System.out.println("ERRO! A linha e a coluna dos vetores em questão, precisam serem iguais.");
+		}
+		
+		} while (i != j);
+		
+		int[][] primeira = new int[i][2];
+		int[][] segunda = new int[2][j];
+		int[][] resultado = new int[i][j];
+		
+		for(int x=0; x<i; x++) {
+			for(int y=0; y<2; y++) {
+				System.out.print("Preencha o primeiro vetor: ");
+				primeira[x][y] = sc.nextInt();
+			}
+		}
+		
+		for(int x=0; x<2; x++) {
+			for(int y=0; y<j; y++) {
+				System.out.print("Preencha o segundo vetor: ");
+				segunda[x][y] = sc.nextInt();
+			}
+		}
+		
+		for(int x=0; x<i; x++) {
+			for(int y=0; y<j; y++) {
+				
+				resultado[x][y] = segunda[x][y] * primeira[x][y];
+			}
+		}
+		
+		for(int x=0; x<i; x++) {
+			for(int y=0; y<j; y++) {
+				System.out.print(resultado[x][y] + "\t");
+			}
+			System.out.println(" ");
+		}
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
